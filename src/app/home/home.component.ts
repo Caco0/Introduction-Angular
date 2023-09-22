5f0399a0ef9d6454b7a17c93dd45271e65a2949d
Rafael Dametto <cacotpds@gmail.com>
2023-09-22 18:19:05 -0300

    Lesson 3

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HousingLocationComponent],
   template: `
     <section>
       <form>
         <input type="text" placeholder="Filtrar por cidade" />
         <button className="primary" type="button">Busca</button>
       </form>
+    </section>
+    <section>
+      <app-housing-location></app-housing-location>
     </section>
   `,
   styleUrls: ['./home.component.css'],
