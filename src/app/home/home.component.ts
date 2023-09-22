fee61db7cd03cc254676408561c80cf8b60dd69b
Rafael Dametto <cacotpds@gmail.com>
2023-09-22 18:30:50 -0300

    Lesson 4 interfaces

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { Housinglocation } from '../housinglocation';
 
 @Component({
   selector: 'app-home',
@@ -19,4 +20,16 @@ import { HousingLocationComponent } from '../housing-location/housing-location.c
   `,
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  readonly baseUrl = 'https://angular.io/assets/images/tutorial/faa';
+  housinglocations: Housinglocation = {
+    id: 999,
+    name: 'Rafael Dametto',
+    city: 'Limeira',
+    state: 'SP',
+    photo: `${this.baseUrl}/house-1.jpg`,
+    availableUnits: 522,
+    wifi: true,
+    laundry: true,
+  };
+}
